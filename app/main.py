b1f4d4c887050d35e38e36ae0add8426b7f23fd1
Евгений Герасин <e.gerasin@bars-open.ru>
2021-11-02 21:40:06 +0300

    main
    Добавли элементы командной строки

--- app/main.py
+++ app/main.py
@@ -1,17 +1,34 @@
+from os import pardir
+import typing
 from lib import lib
 
+import argparse
+
+
 def main():
-    path: str = "/home/evgenii/Work/edu-eschool/"
-    # check = check_exists(path, PATHS)
-    # print(check)
-    lib.create_sourse_files(
-        '/home/evgenii/python-code/other/module-preparator-for-writing-tests/',
-        '/register_AD',
-        True
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c', '--check',
+        help='сhecking modules in a project',
+        action="store_true"
     )
+    parser.add_argument(
+        '-s', '--source',
+        type=str,
+        help='create new module',
+        metavar='<name_module>'
+
+    )
+    parser.add_argument('path', help='path to project', type=str)
+    args = parser.parse_args()
+
+    print(args)
+
+    # проверка проекта на наличие модулей
+    if args.check:
+        lib.check(args.path)
 
 
 if __name__ == "__main__":
     main()
-
-
