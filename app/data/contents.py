573faabd67383a3c0246606e1bd0ea7781d8d724
Евгений Герасин <e.gerasin@bars-open.ru>
2021-11-02 20:54:43 +0300

    main
    Рефакторинг функций

--- app/data/contents.py
+++ app/data/contents.py
@@ -1,35 +1,35 @@
-class Files:
+class FileContent:
 
     LOCATOR_CODE_FILE = (
         "class Locator:\n"
-        "    pass"
+        "\n\tpass\n"
     )
     DATA_CODE_FILE = (
         "class Data:\n"
-        "    pass"
+        "\n\tpass\n"
     )
     TASK_CODE_FILE_WITH_DATA = (
-        "from source.{name_module}.locator import Locator\n"
-        "from source.{name_module}.data import Data\n"
+        "from source{name_module}.locator import Locator\n"
+        "from source{name_module}.data import Data\n"
         "from source.desktop.task import MainPage\n"
         "from utility.exceptions import InvalidTestConditionsException\n"
         "\n"
         "\n"
         "class Task???(MainPage):\n"
-        "    pass"
+        "\n\tpass\n"
     )
     TASK_CODE_FILE = (
-        "from source.{name_module}.locator import Locator\n"
+        "from source{name_module}.locator import Locator\n"
         "from source.desktop.task import MainPage\n"
         "from utility.exeptions import InvalidTestConditionsException\n"
         "\n"
         "\n"
         "class Task???(MainPage):\n"
-        "    pass"
+        "\n\tpass\n"
     )
 
     TEST_CODE_FILE = (
-        "from source.{name_module}.task import ???\n"
+        "from source{name_module}.task import ???\n"
         "from source.description import Description\n"
         "\n"
         "import allure\n"
--- app/main.py
+++ app/main.py
@@ -4,11 +4,10 @@ def main():
     path: str = "/home/evgenii/Work/edu-eschool/"
     # check = check_exists(path, PATHS)
     # print(check)
-    lib.create_objects(
-        '/home/evgenii/Work/bfo-mchs-ui/',
-        'income_forecasting/register_AD',
-        data_file=True,
-        test_file=True
+    lib.create_sourse_files(
+        '/home/evgenii/python-code/other/module-preparator-for-writing-tests/',
+        '/register_AD',
+        True
     )
 
 
