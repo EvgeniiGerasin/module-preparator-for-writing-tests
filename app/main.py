6d519bd00bb6c0467ee94ed90e21b49f377e4b14
Евгений Герасин <e.gerasin@bars-open.ru>
2021-11-03 09:35:38 +0300

    main
    Проведен рефакторинг

--- app/main.py
+++ app/main.py
@@ -8,11 +8,15 @@ import argparse
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('path', help='path to project', type=str)
+    parser.add_argument(
+        '-p', '--path',
+        help='path to directory of project',
+        metavar='<path to directiry>'
+    )
     parser.add_argument(
         '-c', '--check',
         help='сhecking modules in a project',
-        action="store_true"
+        metavar='<path to directiry>'
     )
     parser.add_argument(
         '-s', '--source',
@@ -22,14 +26,19 @@ def main():
     )
     args = parser.parse_args()
 
-    print(args)
-
     # проверка проекта на наличие модулей
     if args.check:
-        lib.check(args.path)
+        lib.check(args.check)
     # создание директории и файлов в каталоге source
     if args.source:
+        # проверка на наличие нужных параметров от пользователя
+        # если таких нет, то выводим сообщение ошибки 
+        # и завершаем работу
+        if not args.path or not args.source:
+            print(parser.error(
+                'try "-p <path_to_directiry> -s <name_module>"'))
         lib.create_sourse_files(args.path, args.source)
+
 
 if __name__ == "__main__":
     main()
